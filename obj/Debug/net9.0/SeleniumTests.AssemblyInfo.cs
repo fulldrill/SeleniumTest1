42c4c477bbcf3a1f22d4693e0d686a0ab68954a1
fulldrill <brightlitandaze4@gmail.com>
2025-08-30 10:16:14 -0400

    readded Jenkinsfile

--- obj/Debug/net9.0/SeleniumTests.AssemblyInfo.cs
+++ obj/Debug/net9.0/SeleniumTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b8176b7d19feb01454b25f0ba64ee5a14b4b795")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa933eae603af6cf5c4da0be12ebf0e0ba928761")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
