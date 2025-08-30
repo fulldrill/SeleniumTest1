6e4dffb60702866d953ae26e06f8dcdb658b13ac
fulldrill <brightlitandaze4@gmail.com>
2025-08-30 09:41:28 -0400

    First Commit

---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    public class GoogleSearchTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");   // Headless mode for Jenkins
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+
+            driver = new ChromeDriver(options);
+        }
+
+        [Test]
+        public void GoogleSearch_ShouldShowResults()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            var searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("Jenkins Selenium .NET");
+            searchBox.Submit();
+
+            Assert.That(driver.Title, Does.Contain("Jenkins"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+    }
+}
